8bd52df8428303cce99218ae7ab937f436b95c4b
s1260031 <s1260031@ilab2dc32.u-aizu.ac.jp>
2019-07-11 17:26:10 +0900

    Initial revision

---
+++ dice.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+
+  int D1,D2;
+   
+  srand((int)time(NULL));
+  
+  D1 = rand()%6+1;
+  for(int i = 0; i < 100; i++){
+    D2 = rand()%6+1;
+  }
+  
+  printf("Rolling the dice...\n");
+  printf("Die 1: %d\n",D1);
+  printf("Die 2: %d\n",D2);
+  printf("Total value: %d\n",D1+D2);
+
+ 
+  return 0;
+}
